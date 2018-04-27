58d91d7c737eba43fa25699f9abd09782d7159a6
Vache Katsadze <vkats15@freeuni.edu.ge>
2018-04-27 22:18:01 +0400

    Added posts from 'db', added make post onfocus overlay

---
+++ scripts/data.js
@@ -0,0 +1,103 @@
+var friends = [ 
+	{
+		name : "Lasha Edilashvili",
+		img : "https://scontent.ftbs5-1.fna.fbcdn.net/v/t1.0-1/c91.218.316.316/s100x100/22405456_1641755512526758_4859570458442661103_n.jpg?_nc_cat=0&oh=7b219956751b07202250276479f86efb&oe=5B67DF30"
+ 	},
+	{
+		name : "Sandro Shanshiashvili",
+		img : "https://scontent.ftbs5-1.fna.fbcdn.net/v/t1.0-1/p100x100/12802843_1030669396979562_4714449103160304201_n.jpg?_nc_cat=0&oh=a8931f7c8589f49f403d493aa60284c8&oe=5B658B63"
+	},
+	{
+		name: "Giorgi Sirbiladze",
+		img : "https://scontent.ftbs5-1.fna.fbcdn.net/v/t1.0-1/p100x100/14606512_1449072395120058_6461035292057782952_n.jpg?_nc_cat=0&oh=62867e35fa8d8eb844ff6331f1107536&oe=5B94BD80"
+	},
+	{
+		name : "ლუკა მაზმიშვილი",
+		img : "https://scontent.ftbs5-1.fna.fbcdn.net/v/t1.0-1/p100x100/20430036_1658356570872052_5519126694741594934_n.jpg?_nc_cat=0&oh=0c1a9e4fb41fff45ed41723b659774a8&oe=5B575DE5"
+	},
+	{
+		name : "თამუნა გარდაფხაძე",
+		img : "https://scontent.ftbs5-1.fna.fbcdn.net/v/t1.0-1/c35.97.467.467/s100x100/18403268_1523511274348003_6120404876925640721_n.jpg?_nc_cat=0&oh=d48e6f41cb6850742e192e368b268901&oe=5B5DC1FD"
+	},
+	{
+		name : "Nika Jaghiashvili",
+		img : "https://scontent.ftbs5-1.fna.fbcdn.net/v/t1.0-1/c29.0.100.100/p100x100/10354686_10150004552801856_220367501106153455_n.jpg?_nc_cat=0&oh=719b025ac01fe5a8baf536315c4afb3b&oe=5B66BD77"
+	},
+	{
+		name : "Otar Chekurishvili",
+		img : "https://scontent.ftbs5-1.fna.fbcdn.net/v/t1.0-1/p160x160/29793106_1839262909431041_2941807771739226112_n.jpg?_nc_cat=0&oh=d4b51ac61b0f6877b591af087f2540cf&oe=5B9CFB40"
+	},
+	{
+		name : "Nodo Sanaia",
+		img : "https://scontent.ftbs5-1.fna.fbcdn.net/v/t1.0-1/p100x100/18119035_1476655045718548_3695045088900175596_n.jpg?_nc_cat=0&oh=c605a8c69ce3be02595ba687a072eeb5&oe=5B5FD931"
+	},
+	{
+		name : "Vazha Beridze",
+		img : "https://scontent.ftbs5-1.fna.fbcdn.net/v/t1.0-1/c22.0.100.100/p100x100/30127753_2381876161838096_1330335054437548032_n.jpg?_nc_cat=0&oh=24c3e77a1b0a75c96f0570e52f4120df&oe=5B5C4DC6"
+	},
+	{
+		name : "Nika Sakandelidze",
+		img : "https://scontent.ftbs5-1.fna.fbcdn.net/v/t1.0-1/p100x100/25398996_946871788813214_845149403034714172_n.jpg?_nc_cat=0&oh=f455db239d3cfddb4c5dc9475dff17d6&oe=5B4F4EB1"
+	},
+	{
+		name : "Sandro Jorbenadze",
+		img : "https://scontent.ftbs5-1.fna.fbcdn.net/v/l/t1.0-1/p100x100/10478027_728415823900733_7261860373556099499_n.jpg?_nc_cat=0&oh=272bb81c12cb985e95045b9517e68256&oe=5B5E49D2"
+	},
+	{
+		name : "Levan Karanadze",
+		img : "https://scontent.ftbs5-1.fna.fbcdn.net/v/t1.0-1/c29.0.100.100/p100x100/10354686_10150004552801856_220367501106153455_n.jpg?_nc_cat=0&oh=719b025ac01fe5a8baf536315c4afb3b&oe=5B66BD77"
+	},
+	{
+		name : "Aleko Mosidze",
+		img : "https://scontent.ftbs5-1.fna.fbcdn.net/v/t1.0-1/p100x100/15230761_1339594046059124_8174553807554465925_n.jpg?_nc_cat=0&oh=baab6620c0b3bdf16dd4d1d697ed1657&oe=5B5E22C3"
+	},
+]
+
+
+var posts = [ 
+ 	{
+		author : "MKBHD",
+		avatar : "https://scontent.ftbs5-1.fna.fbcdn.net/v/t1.0-1/p100x100/15095027_1188901737822527_4758136153806777387_n.png?_nc_cat=0&oh=0b9f8bc54f9755ab10c1336d5dc37d34&oe=5B9BCB2D",
+		img : "https://scontent.ftbs5-1.fna.fbcdn.net/v/t1.0-9/31178446_1766938333352195_6504751725532692770_n.jpg?_nc_cat=0&oh=c205f7aa973e228f88337f1d8de3bddc&oe=5B9C6DB3",
+		likes : "722",
+		commentShare : "2.1K Comments &nbsp; 709 Shares",
+		text: "NEW VIDEO - Huawei P20 Pro Review: The Triple Camera Smartphone!",
+		time: "17 mins ·",
+ 	},
+	{
+		author : "9GAG",
+		avatar : "https://scontent.ftbs5-1.fna.fbcdn.net/v/t1.0-1/p100x100/17903437_10155554645366840_7299462485287843175_n.png?_nc_cat=0&oh=0f9dc551f79cb1645fc8aa6e37bf205e&oe=5B96B7C2",
+		img : "https://scontent.ftbs5-1.fna.fbcdn.net/v/t1.0-9/30716346_10157563389826840_6002404966225472799_n.jpg?_nc_cat=0&oh=98ed15e12e46cf41728ca9d88b7bf4c4&oe=5B5E7AA2",
+		likes : "670",
+		commentShare : "1K Comments &nbsp; 523 Shares",
+		text: "Bromance goal. By Joe Heenan | TW",
+		time: "40 mins ·",
+ 	},
+ 	{
+		author : "Sandro Shanshiashvili",
+		avatar : "https://scontent.ftbs5-1.fna.fbcdn.net/v/t1.0-1/p100x100/12802843_1030669396979562_4714449103160304201_n.jpg?_nc_cat=0&oh=a8931f7c8589f49f403d493aa60284c8&oe=5B658B63",
+		img : "",
+		likes : "11",
+		commentShare : "30 Comments",
+		text: "გიყვარვარ თუ გკიდივარ?",
+		time: "2 hrs ·",
+ 	},
+	{
+		author : "Lasha Edilashvili",
+		avatar : "https://scontent.ftbs5-1.fna.fbcdn.net/v/t1.0-1/c91.218.316.316/s100x100/22405456_1641755512526758_4859570458442661103_n.jpg?_nc_cat=0&oh=7b219956751b07202250276479f86efb&oe=5B67DF30",
+		img : "https://scontent.ftbs5-1.fna.fbcdn.net/v/t1.0-9/31239340_759686507573538_4142225833222922635_n.png?_nc_cat=0&oh=7cbd0c7adad23e5ccf33066ce1f42eef&oe=5B5809CC",
+		likes : "5",
+		commentShare : "3 Comments &nbsp; 1 Shares",
+		text: "მაგარი ფეხბურთელია, მე სელტიკის დაწყებაზე ვდებ ფულს",
+		time: "22 hrs ·",
+ 	},
+ 	{
+		author : "8FACT",
+		avatar : "https://scontent.ftbs5-1.fna.fbcdn.net/v/t1.0-1/p200x200/16114234_532575236936615_203012657497650387_n.png?_nc_cat=0&oh=a54092ec4b853f2f8179ba06a345ccdf&oe=5B64F588",
+		img : "https://scontent.ftbs5-1.fna.fbcdn.net/v/t1.0-9/31171189_718394815021322_275532432897340235_n.png?_nc_cat=0&oh=0379f3aca6225883239ca7e6cca38f29&oe=5B617D97",
+		likes : "1.3K",
+		commentShare : "16 Comments &nbsp; 59 Shares",
+		text: "Time flies.",
+		time: "Yesterday at 7:14am ·",
+ 	},
+ ]
